4d612a314a22492c0c4c50c1cc4d0f9ea3057b25
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-02-28 22:55:53 -0500

    Initial Commit

---
+++ SideMenuBar/SideMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuBarApp.swift
+//  SideMenuBar
+//
+//  Created by Juan Sebastian Orozco Buitrago on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
